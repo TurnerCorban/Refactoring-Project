5ab567acbb0ecabb34f31b8f35c0873956a805cd
TurnerCorban <cturner25@cub.uca.edu>
2025-10-10 11:24:38 -0500

    - Moved registration_data.json to data folder in edu.registration.utility - Refactored Enrollment.java from class to record (fixed file reading bug)

--- src/main/java/edu/uca/registration/utility/Config.java
+++ src/main/java/edu/uca/registration/utility/Config.java
@@ -1,22 +1,11 @@
 package edu.uca.registration.utility;
 
 public class Config {
+    private static final String datapath = "src/main/java/edu/uca/registration/data/"; //data folder
+    private static final String filename = "registration_data.json"; //json save file
+    private static final String filepath = datapath + filename;
+
     public static String getDataFilePath() {
-        return System.getProperty("data.file", "registration_data.json");
-    }
-
-    public static boolean isDemoMode() {
-        String demoProperty = System.getProperty("demo.mode");
-        if (demoProperty == null) {
-            return "true".equalsIgnoreCase(demoProperty);
-        }
-
-        for (String arg : java.lang.management.ManagementFactory.getRuntimeMXBean().getInputArguments()) {
-            if ("--demo".equals(arg)) {
-                return true;
-            }
-        }
-
-        return false;
+        return System.getProperty("data.file", filepath);
     }
 }
